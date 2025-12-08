660ad6543123bb15664a699c73e0d8663993a450
Saalim Quadri <danascape@gmail.com>
2025-12-08 08:48:04 +0530

    feat: Bump release to 1.0.1
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdk = 23
         targetSdk = 36
 
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         setProperty("archivesBaseName", "Messages-v${versionName}")
 
